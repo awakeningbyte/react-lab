966dd6633a831017f72935c2e30effd9a4cbb1ea
George C <gchen@awakeningbyte.com>
2016-09-24 21:29:35 -0700

    css file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
 import ProductSelection from "./product_selection"
-
+import css from "./style.css";
 class AppleStore extends Component {
 	render() {
 		return <ProductSelection />
--- src/product_selection/index.js
+++ src/product_selection/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {render} from "react-dom";
 import Filter from "./filter";
 import ProductList from "./product_list";
+import css from "./style.css"
 
 class ProductSelection extends Component {
 
