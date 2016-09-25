e5bb26af7a18a7fce973522d8b7b95e159481428
George C <gchen@awakeningbyte.com>
2016-09-24 20:47:25 -0700

    add product filter and product list to product selection page

---
+++ src/product_selection/filter/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {render} from "react-dom";
+
+class ProductFilter extends Component {
+	render() {
+		return		<div>Filter</div>
+	}
+}
+
+export default ProductFilter;
--- src/product_selection/index.js
+++ src/product_selection/index.js
@@ -1,11 +1,15 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
+import Filter from "./filter";
+import ProductList from "./product_list";
 
 class ProductSelection extends Component {
 
 	render() {
 		return	<div> 
 				<h2> Buy Macbook Pro</h2>
+				<Filter />
+				<ProductList/>
 			</div>
 	}
 }
