faa90bf4f64bd162f948d2b5cff2ab3931141345
George C <gchen@awakeningbyte.com>
2016-09-25 08:27:11 -0700

    restructure project

--- apple_store/src/product_selection/filter/index.js
+++ apple_store/src/product_selection/filter/index.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
-
+import macbook from "../../../assets/macbook_large.svg"; 
 class ProductFilter extends Component {
 	render() {
-		return		<div>Filter</div>
+		return		<div>
+				<img src={macbook}/>
+				<span>MacBook</span>
+			</div>
 	}
 }
 
--- apple_store/webpack.config.js
+++ apple_store/webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
 			{ 
 				test: /\.scss$/,
 				loader: 'style!css!sass?outputStyle=compressed'
+			},
+			{
+				test: /\.svg$/,
+				loader: 'url'
 			}
 
 		]
