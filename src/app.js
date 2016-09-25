0f1836f07225ab5935659b713a9aab3e782cce0e
George C <gchen@awakeningbyte.com>
2016-09-24 20:16:35 -0700

    restructure app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {render} from "react-dom";
+
+class AppleStore extends Component {
+	render() {
+		return <div> "" </div>
+	}
+
+}
+
+
+render(<AppleStore />, document.getElementById("content"));
