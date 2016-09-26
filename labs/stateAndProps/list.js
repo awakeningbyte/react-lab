9cf4e6e6c42b4ceae281aafd2172b277da984a9a
George C <gchen@awakeningbyte.com>
2016-09-25 20:35:45 -0700

    model list component

--- labs/stateAndProps/list.js
+++ labs/stateAndProps/list.js
@@ -3,10 +3,17 @@ import {render} from 'react-dom';
 
 class List extends Component {
 	render() {
-		return <div>
-			<ul>
-				list here
+		let content;
+		if (this.props.models.length > 0) {
+			content = <ul>
+			{this.props.models.map( ({model,year,price},i) => <li key={i}>{year} {model}: ${price} </li>)}
 			</ul>
+		} else {
+			<div> No model found</div>
+		}
+
+		return <div>
+		{content}
 		</div>
 	}
 }
